20fe9bb7a0e41a65135ccd94d8ff286b264d2fc8
github username <github email address>
2019-04-23 18:42:01 +0200

    programming language hash converted

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+	new_hash = {}
+	languages.each do |style, lang_hash|
+		lang_hash.each do |lang_name, lang_type|
+			if new_hash[lang_name] == nil
+				new_hash[lang_name] = lang_type
+				new_hash[lang_name][:style] = []
+			end
+			# this if checks if lang_name exists in new_hash. if it doesn't 
+			# it creates lang_type and style keys for each lang_name
+			new_hash[lang_name][:style] << style
+			# then we can push the style to the empty array for each language
+		end
+	end
+	new_hash
 end
